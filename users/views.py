112eb7d025909ea0d78579998941cb7c8e4cc941
Habibul Bashar <habibulbasharcse@gmail.com>
2025-09-06 13:07:02 +0600

    found bug

--- users/views.py
+++ users/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def show_user(request) :
     return HttpResponse("hello users")
+
+def showUserId(request, id):
+    return HttpResponse(f"User Id is {id}")
+
