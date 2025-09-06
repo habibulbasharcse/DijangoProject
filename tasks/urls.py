ec192ceeb382c651812b5de58704ae89ebf2002a
Habibul Bashar <habibulbasharcse@gmail.com>
2025-09-06 12:55:12 +0600

    bug fixes on urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,6 @@ from tasks.views import show_task ,showTaskWithPara ,txt
 
 urlpatterns =[
     path('show_task/', show_task),
-    path('showTaskWithPara/<int:id>/', showTaskWithPara)
+    path('showTaskWithPara/<int:id>/', showTaskWithPara),
     path('txt/<int:id>/', txt)
 ]
