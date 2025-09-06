b97e23fb4d7a43a914c1044a4772c5e02438870b
Habibul Bashar <habibulbasharcse@gmail.com>
2025-09-06 13:16:04 +0600

    fixes bug on user urls

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from users.views import show_user
+from users.views import show_user, showUserId
 urlpatterns =[
-    path('show_user/', show_user)
-    path('showUserI/<id>/', showUserI)
+    path('show_user/', show_user),
+    path('showUserId/<id>/', showUserId)
 ]
